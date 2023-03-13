c6197dd48b1bf9cc8d2fbe21a88098ca3117083f
blackseif <108239975+8lackseif@users.noreply.github.com>
2023-03-13 16:01:16 +0100

    make prettier the code

--- src/main.py
+++ src/main.py
@@ -5,15 +5,11 @@ import os
 import animdl.core.config
 import requests
 
-
-def getNumberCaps(anime: str)->tuple[int,str]:
+def getNumberCaps(anime: str)->int:
     url = 'https://graphql.anilist.co'
     query = '''
            query songImage($userPreferred: String, $type: MediaType) {
                Media (search : $userPreferred, type: $type) {
-                   title{
-                        userPreferred
-                   }
                    episodes
                }
            }
@@ -24,47 +20,44 @@ def getNumberCaps(anime: str)->tuple[int,str]:
     }
     response = requests.post(url, json={'query': query, 'variables': variables})
     print(response.json())
-    return response.json()['data']['Media']['episodes'],response.json()['data']['Media']['title']['userPreferred']
+    return response.json()['data']['Media']['episodes']
 
-def getIndex(anime: str)->str:
-    max = 0
-    output = subprocess.run('animdl search made-in-abyss', stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE).stderr.decode('utf-16')
+def getList(anime: str)-> list[str]:
+    animeList = []
+    anime = anime.replace(' ','-')
+    output = subprocess.run('animdl search ' + anime, stdout=subprocess.PIPE,stderr=subprocess.PIPE).stderr.decode('utf-16')
     for line in output.splitlines():
         if line[0].isdigit():
-            if int(line[0]) > max:
-                max = int(line[0])
-    return str(max)
-def main():
-    print('escribe un anime: ')
-    name = input()
-    max,title = getNumberCaps(name)
-    print(title)
-    title = title.replace(' ' ,'-')
-    ind = 1
-    x = ''
-    salir = 0
+            animeList.append(line[4:line.find('/')])
+    return animeList
+
+def play(firstCap: int, index: int, name:str):
+    # calculate the range
+    lastCap = getNumberCaps(name)
+    if lastCap - firstCap + 1 > 25:
+        lastCap = firstCap + 24
+    #search for the caps
+    list = ''
     while True:
         for site in animdl.core.config.SITE_URLS:
             animdl.core.config.DEFAULT_CONFIG['default_provider'] = site
-            print('grabbing cap ' + str(ind))
+            print('grabbing cap ' + str(firstCap))
             print(animdl.core.config.DEFAULT_CONFIG['default_provider'])
-            output = subprocess.run('animdl grab -r '+ str(ind) + ' --index '+ getIndex(title) +' '+ title,stdout=subprocess.PIPE,stderr=subprocess.DEVNULL).stdout.decode('utf-8')
-            print(output)
-            output = output[:output.rfind('}')+1]
+            output = subprocess.run('animdl grab -r ' + str(firstCap) + ' --index ' + str(index) + ' ' + name.replace(' ','-'),
+                                    stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.decode('utf-8')
+            output = output[:output.rfind('}') + 1]
             try:
                 episode = json.loads(output)
-                x += ' ' + episode['streams'][0]['stream_url']
+                list += ' ' + episode['streams'][0]['stream_url']
                 break
             except:
                 pass
-        ind += 1
-        if ind > max:
+        firstCap += 1
+        if firstCap > lastCap:
             break
-
-    os.system('mpv' + x)
-
-
+    #play
+    os.system('mpv' + list)
+def main():
 
     sg.theme('DarkAmber')
 
@@ -72,17 +65,17 @@ def main():
               [sg.Text('Enter something on Row 2'), sg.InputText()],
               [sg.Button('Ok'), sg.Button('Cancel')]]
 
-    #
-    #
-    #
-    #window = sg.Window('reproductordeanimequepasadecapituloautomaticamente',layout)
+    window = sg.Window('reproductordeanimequepasadecapituloautomaticamente',layout)
 
-    #while True:
-    #   event, values = window.read()
-    #   if event is not None and values is not None:
-    #       window['-change-'].update(values[0])
+    while True:
+       event, values = window.read()
+       if event is not None and values is not None:
+           window['-change-'].update(values[0])
 
-    #    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
-    #        break
-    #window.close()
+       if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+            break
+    window.close()
+
+
+
 main()
