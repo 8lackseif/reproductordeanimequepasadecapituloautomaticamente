3a1f506f9460dffd3c1bcf41c7f07e5d0c83b70c
blackseif <108239975+8lackseif@users.noreply.github.com>
2023-03-13 20:04:24 +0100

    base of GUI

--- src/main.py
+++ src/main.py
@@ -6,10 +6,17 @@ import animdl.core.config
 import requests
 
 def getNumberCaps(anime: str)->int:
+    #if it is a movie
+    if "Movie" in anime:
+        return 1
+
     url = 'https://graphql.anilist.co'
     query = '''
-           query songImage($userPreferred: String, $type: MediaType) {
+           query episodes($userPreferred: String, $type: MediaType) {
                Media (search : $userPreferred, type: $type) {
+                   title{
+                        userPreferred
+                   }
                    episodes
                }
            }
@@ -19,30 +26,30 @@ def getNumberCaps(anime: str)->int:
         'type': 'ANIME'
     }
     response = requests.post(url, json={'query': query, 'variables': variables})
-    print(response.json())
+    if response.json()['data']['Media']['episodes'] is None:
+        return 0
     return response.json()['data']['Media']['episodes']
 
 def getList(anime: str)-> list[str]:
     animeList = []
     anime = anime.replace(' ','-')
-    output = subprocess.run('animdl search ' + anime, stdout=subprocess.PIPE,stderr=subprocess.PIPE).stderr.decode('utf-16')
+    output = subprocess.run('animdl search ' + anime, stdout=subprocess.PIPE,stderr=subprocess.PIPE).stderr.decode('utf-8')
     for line in output.splitlines():
         if line[0].isdigit():
-            animeList.append(line[4:line.find('/')])
+            animeList.append(line[3:line.find('/') - 1])
     return animeList
 
 def play(firstCap: int, index: int, name:str):
     # calculate the range
     lastCap = getNumberCaps(name)
-    if lastCap - firstCap + 1 > 25:
+    if lastCap - (firstCap + 1) > 25:
         lastCap = firstCap + 24
     #search for the caps
     list = ''
     while True:
         for site in animdl.core.config.SITE_URLS:
             animdl.core.config.DEFAULT_CONFIG['default_provider'] = site
-            print('grabbing cap ' + str(firstCap))
-            print(animdl.core.config.DEFAULT_CONFIG['default_provider'])
+            print('grabbing cap ' + str(firstCap) + ' from ' + animdl.core.config.DEFAULT_CONFIG['default_provider'])
             output = subprocess.run('animdl grab -r ' + str(firstCap) + ' --index ' + str(index) + ' ' + name.replace(' ','-'),
                                     stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.decode('utf-8')
             output = output[:output.rfind('}') + 1]
@@ -56,24 +63,56 @@ def play(firstCap: int, index: int, name:str):
         if firstCap > lastCap:
             break
     #play
-    os.system('mpv' + list)
+    if len(list) == 0:
+        print("failed grabbing stream urls")
+    else:
+        os.system('mpv' + list)
 def main():
 
     sg.theme('DarkAmber')
 
-    layout = [[sg.Text('Some text on Row 1',key='-change-')],
-              [sg.Text('Enter something on Row 2'), sg.InputText()],
-              [sg.Button('Ok'), sg.Button('Cancel')]]
+    search_column = [
+        [sg.InputText(size=(55,1), key="-ANIME-"), sg.Button("Search",size=(5,1))],
+        [sg.Listbox(values=[],enable_events=True,size=(60,20),key="-SEARCH-")]
+    ]
+
+    cap_column = [
+        [sg.Text("caps list: ", key="-TITLE-")],
+        [sg.Listbox(values=[],enable_events=True,size=(40,20),key="-CAPS-")]
+    ]
+
+    layout = [
+        [sg.Column(search_column),sg.VSeperator(),sg.Column(cap_column)]
+    ]
 
     window = sg.Window('reproductordeanimequepasadecapituloautomaticamente',layout)
 
-    while True:
-       event, values = window.read()
-       if event is not None and values is not None:
-           window['-change-'].update(values[0])
+    currentAnime = ""
+    currentAnimeList = []
+    currentIndex = 0
 
-       if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
             break
+        elif event == "Search":
+            currentAnimeList = getList(values["-ANIME-"])
+            window.Element("-SEARCH-").update(currentAnimeList)
+        elif event == "-SEARCH-":
+            currentAnime = values[event][0]
+            window["-TITLE-"].update(currentAnime + ": ")
+            caps = getNumberCaps(values[event][0])
+            episodes = []
+            for i in range(1,caps + 1):
+                episodes.append("episode " + str(i))
+            window["-CAPS-"].update(episodes)
+        elif event == "-CAPS-":
+            for n in currentAnimeList:
+                if currentAnime == n:
+                    currentIndex = n
+                    break
+            play(int(values[event][0][len(values[event][0])-1]), currentIndex, currentAnime)
+
     window.close()
 
 
