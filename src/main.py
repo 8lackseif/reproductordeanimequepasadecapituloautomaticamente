0f5b6c6d095987aaf9c77f4315578abcccdf9cf6
blackseif <108239975+8lackseif@users.noreply.github.com>
2023-03-22 22:11:48 +0100

    change gui async with the mpv player, now you can open more than 1 mpv

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-
-import threading
+import asyncio
 import PySimpleGUI as sg
 import subprocess
 import json
-import os
 import animdl.core.config
 import requests
 import animdl.core.package_resolver
@@ -13,15 +11,14 @@ capitulos = {
 }
 
 
-def getNumberCaps(anime: str)->int:
-    #if it is a movie
+def getNumberCaps(anime: str) -> int:
+    # if it is a movie
     if "Movie" in anime:
         return 1
 
     if "Part" in anime and ":" in anime:
         if "Part" in anime[:anime.find(":")]:
             anime = anime[:anime.find("Part")] + anime[anime.rfind(":"):]
-
 
     url = 'https://graphql.anilist.co'
     query = '''
@@ -38,21 +35,26 @@ def getNumberCaps(anime: str)->int:
         'userPreferred': anime,
         'type': 'ANIME'
     }
+
     response = requests.post(url, json={'query': query, 'variables': variables})
     if response.status_code == 404 or response.json()['data']['Media']['episodes'] is None:
         return 0
     return response.json()['data']['Media']['episodes']
 
-def getList(anime: str)-> list[str]:
+
+async def getList(anime: str) -> list[str]:
     animeList = []
-    anime = anime.replace(' ','-')
-    output = subprocess.run('animdl search ' + anime, stdout=subprocess.PIPE, shell=True,stderr=subprocess.PIPE).stderr.decode('utf-8')
+    anime = anime.replace(' ', '-')
+    player = await asyncio.create_subprocess_exec('animdl', 'search',anime,
+                                                  stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout,stderr = await player.communicate()
+    output = stderr.decode().strip()
     for line in output.splitlines():
         if line[0].isdigit():
             animeList.append(line[3:line.find('/') - 1])
     return animeList
 
-def scrap(currentEpisode: int, index: int, animeName: str):
+async def scrap(currentEpisode: int, index: int, animeName: str):
     for site in animdl.core.config.SITE_URLS:
         animdl.core.config.DEFAULT_CONFIG['default_provider'] = site
         output = subprocess.run('animdl grab -r ' + str(currentEpisode) + ' --index ' + str(index) + ' ' + animeName,
@@ -66,23 +68,18 @@ def scrap(currentEpisode: int, index: int, animeName: str):
             pass
 
 
-def play(firstCap: int, index: int, searchName:str, realName: str):
+async def play(firstCap: int, index: int, searchName: str, realName: str):
     # calculate the range
     lastCap = getNumberCaps(realName)
-    threads = []
     if lastCap - (firstCap + 1) > 25:
         lastCap = firstCap + 24
 
-    animeName = searchName.replace(' ','-')
+    animeName = searchName.replace(' ', '-')
 
     sg.Popup('Warning', 'The program will now start scraping. This may take a while.')
 
     for i in range(firstCap, lastCap + 1):
-        threads.append(threading.Thread(target=scrap, args=(i,index,animeName)))
-        threads[len(threads) - 1].start()
-
-    for t in threads:
-        t.join()
+        await scrap(i,index,animeName)
 
     with open("playlist.m3u", "w") as f:
         for i in range(firstCap, lastCap + 1):
@@ -92,30 +89,29 @@ def play(firstCap: int, index: int, searchName:str, realName: str):
                 f.write(f"" + url + "\n")
             except:
                 pass
-    #play
-    subprocess.run('mpv --no-terminal playlist.m3u',shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    # play
+    player = await asyncio.create_subprocess_exec('mpv', '--no-terminal', 'playlist.m3u',
+                                                  stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
 
-
-def mainWindow():
-
+async def mainWindow():
     sg.theme('DarkAmber')
 
     search_column = [
-        [sg.InputText(size=(50,1), key="-ANIME-"), sg.Button("Search",size=(8,1))],
-        [sg.Listbox(values=[], enable_events=True, size=(60,20), key="-SEARCH-")]
+        [sg.InputText(size=(50, 1), key="-ANIME-"), sg.Button("Search", size=(8, 1))],
+        [sg.Listbox(values=[], enable_events=True, size=(60, 20), key="-SEARCH-")]
     ]
 
     cap_column = [
-        [sg.Text("caps list: ",size=(30,1), key="-TITLE-",expand_x=True)],
-        [sg.Listbox(values=[],enable_events=True,size=(40,20),key="-CAPS-")]
+        [sg.Text("caps list: ", size=(30, 1), key="-TITLE-", expand_x=True)],
+        [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-CAPS-")]
     ]
 
     layout = [
-        [sg.Column(search_column),sg.VSeperator(),sg.Column(cap_column)]
+        [sg.Column(search_column), sg.VSeperator(), sg.Column(cap_column)]
     ]
 
-    window = sg.Window('reproductordeanimequepasadecapituloautomaticamente',layout, font=('Arial',11))
+    window = sg.Window('reproductordeanimequepasadecapituloautomaticamente', layout, font=('Arial', 11))
 
     searchContent = ""
     currentAnime = ""
@@ -132,11 +128,11 @@ def mainWindow():
         elif event == "Search":
             try:
                 searchContent = values["-ANIME-"]
-                currentAnimeList = getList(values["-ANIME-"])
+                currentAnimeList = await getList(values["-ANIME-"])
                 window.Element("-SEARCH-").update(currentAnimeList)
             except:
                 pass
-        #anime selected display episodes
+        # anime selected display episodes
         elif event == "-SEARCH-":
             try:
                 currentAnime = values[event][0]
@@ -150,7 +146,7 @@ def mainWindow():
                 window["-CAPS-"].update(episodes)
             except:
                 pass
-        #episode selected start play
+        # episode selected start play
         elif event == "-CAPS-":
             i = 0
             for n in currentAnimeList:
@@ -159,12 +155,13 @@ def mainWindow():
                     currentIndex = i
                     break
             try:
-                play(int(values[event][0][values[event][0].rfind(' ')+1:len(values[event][0])]), currentIndex, searchContent, currentAnime)
+                await play(int(values[event][0][values[event][0].rfind(' ') + 1:len(values[event][0])]), currentIndex,
+                           searchContent, currentAnime)
             except:
                 pass
     window.close()
 
+
 if __name__ == '__main__':
-    mainWindow()
-
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(mainWindow())
